f4bd7eb41ca17db3bc1c611240902b4122c165be
Hudson Monteiro <hudsonmonteiro2011!gmail.com>
2021-05-22 23:49:53 -0300

    adicionei as letras que o adversário irá jpgar

--- jogo_1.py
+++ jogo_1.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -8,7 +9,23 @@ pygame.display.set_caption('Diploma Battle')
 
 background = pygame.image.load('imagens/fachada_insper.jpg').convert()
 background_scale = pygame.transform.scale(background, (568, 513))
-#623x550
+letra_I = pygame.image.load('imagens/letra_i.jpeg').convert_alpha()
+letra_D = pygame.image.load('imagens/letra_d.jpeg').convert_alpha()
+letra_I_menor = pygame.transform.scale(letra_I, (50, 50))
+letra_D_menor = pygame.transform.scale(letra_D, (50, 50))
+
+letra_ix = 225
+letra_iy = 0
+
+letra_dx = 225
+letra_dy = 0
+
+letra_i_speedx = random.randint(-3,3)
+letra_i_speedy =  random.randint(2,9)
+
+letra_d_speedx = random.randint(-3,3)
+letra_d_speedy =  random.randint(2,9)
+
 
 game = True
 
@@ -19,13 +36,18 @@ while game:
         
         if event.type == pygame.QUIT:
             game = False
-        if event.type == pygame.KEYDOWN:
-            game = False
+
+    letra_ix += letra_i_speedx
+    letra_iy += letra_i_speedy
+
+    letra_dx += letra_d_speedx
+    letra_dy += letra_d_speedy
 
     
     window.fill((0, 0, 0))  # Preenche com a cor preta
     window.blit(background_scale, (-20, 0))
-
+    window.blit(letra_I_menor,(letra_ix,letra_iy))
+    window.blit(letra_D_menor,(letra_dx,letra_dy))
     #Atualiza jogo
     pygame.display.update()  
 
