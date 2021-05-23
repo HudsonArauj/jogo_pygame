65f108c28fc40444fe522314dd4bca1957b9d38b
DomRuanSuzano <dom.ruan.drs@gmail.com>
2021-05-22 23:05:14 -0400

    Coloquei a velociade certa nas letras e que elas reiniciassem quando saissem da tela

--- jogo_1.py
+++ jogo_1.py
@@ -4,15 +4,24 @@ import random
 pygame.init()
 
 #tela principal
-window = pygame.display.set_mode((450, 500))
+comprimento = 500
+altura = 450
+
+comprimento_fundo = 568
+altura_fundo = 513
+
+comprimento_letras = 50
+altura_letras = 50
+
+window = pygame.display.set_mode((comprimento, altura))
 pygame.display.set_caption('Diploma Battle')
 
 background = pygame.image.load('imagens/fachada_insper.jpg').convert()
-background_scale = pygame.transform.scale(background, (568, 513))
+background_scale = pygame.transform.scale(background, (comprimento_fundo, altura_fundo))
 letra_I = pygame.image.load('imagens/letra_i.jpeg').convert_alpha()
 letra_D = pygame.image.load('imagens/letra_d.jpeg').convert_alpha()
-letra_I_menor = pygame.transform.scale(letra_I, (50, 50))
-letra_D_menor = pygame.transform.scale(letra_D, (50, 50))
+letra_I_menor = pygame.transform.scale(letra_I, (comprimento_letras, altura_letras))
+letra_D_menor = pygame.transform.scale(letra_D, (comprimento_letras, altura_letras))
 
 letra_ix = 225
 letra_iy = 0
@@ -26,11 +35,14 @@ letra_i_speedy =  random.randint(2,9)
 letra_d_speedx = random.randint(-3,3)
 letra_d_speedy =  random.randint(2,9)
 
-
 game = True
+
+clock = pygame.time.Clock() #Ajustando a velocidade
+FPS = 30
 
 #Loop principal
 while game:
+    clock.tick(FPS)
 
     for event in pygame.event.get():
         
@@ -43,6 +55,16 @@ while game:
     letra_dx += letra_d_speedx
     letra_dy += letra_d_speedy
 
+    if letra_iy > altura or letra_ix > comprimento or letra_ix < 0:
+        letra_ix = 225
+        letra_iy = 0
+        letra_i_speedx = random.randint(-3,3)
+        letra_i_speedy =  random.randint(2,9)
+    if letra_dy > altura or letra_dx > comprimento or letra_dx < 0:
+        letra_dx = 225
+        letra_dy = 0
+        letra_d_speedx = random.randint(-3,3)
+        letra_d_speedy =  random.randint(2,9)
     
     window.fill((0, 0, 0))  # Preenche com a cor preta
     window.blit(background_scale, (-20, 0))
