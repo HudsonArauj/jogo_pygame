ffe76606aa43d751003d31114a21fba893fa8e57
DomRuanSuzano <dom.ruan.drs@gmail.com>
2021-06-02 09:42:46 -0400

    Organizei as imagens e sons em dicionários

--- jogo_1.py
+++ jogo_1.py
@@ -18,21 +18,22 @@ altura_jogador = 90
 window = pygame.display.set_mode((comprimento, altura))
 pygame.display.set_caption('Diploma Battle')
 
-background = pygame.image.load('imagens/fachada_insper.jpg').convert()
-background = pygame.transform.scale(background, (comprimento_fundo, altura_fundo))
-letra_I_imagem = pygame.image.load('imagens/letra_i.png').convert_alpha()
-letra_D_imagem = pygame.image.load('imagens/letra_d.png').convert_alpha()
-letra_I_imagem = pygame.transform.scale(letra_I_imagem, (comprimento_letras, altura_letras))
-letra_D_imagem = pygame.transform.scale(letra_D_imagem, (comprimento_letras, altura_letras))
-jogador_imagem = pygame.image.load('imagens/prof1.png').convert_alpha()
-jogador_imagem = pygame.transform.scale(jogador_imagem, (comprimento_jogador, altura_jogador))
-aviao_imagem = pygame.image.load('imagens/aviao_branco.png').convert_alpha()
-aviao_imagem = pygame.transform.scale(aviao_imagem, (comprimento_letras, altura_letras))
+recursos = {}
+recursos['background'] = pygame.image.load('imagens/fachada_insper.jpg').convert()
+recursos['background'] = pygame.transform.scale(recursos['background'], (comprimento_fundo, altura_fundo))
+recursos['letra_I_imagem'] = pygame.image.load('imagens/letra_i.png').convert_alpha()
+recursos['letra_D_imagem'] = pygame.image.load('imagens/letra_d.png').convert_alpha()
+recursos['letra_I_imagem'] = pygame.transform.scale(recursos['letra_I_imagem'], (comprimento_letras, altura_letras))
+recursos['letra_D_imagem'] = pygame.transform.scale(recursos['letra_D_imagem'], (comprimento_letras, altura_letras))
+recursos['jogador_imagem'] = pygame.image.load('imagens/prof1.png').convert_alpha()
+recursos['jogador_imagem'] = pygame.transform.scale(recursos['jogador_imagem'], (comprimento_jogador, altura_jogador))
+recursos['aviao_imagem'] = pygame.image.load('imagens/aviao_branco.png').convert_alpha()
+recursos['aviao_imagem'] = pygame.transform.scale(recursos['aviao_imagem'], (comprimento_letras, altura_letras))
 pygame.mixer.music.load('sons/fundodojogo.mp3')
 pygame.mixer.music.set_volume(0.4)
-hit_professor = pygame.mixer.Sound('sons/atingeprof.wav')
-lancamento_aviao = pygame.mixer.Sound('sons/lançamento.wav')
-hit_aluno = pygame.mixer.Sound('sons/atingealuno.wav')
+recursos['hit_professor'] = pygame.mixer.Sound('sons/atingeprof.wav')
+recursos['lancamento_aviao'] = pygame.mixer.Sound('sons/lançamento.wav')
+recursos['hit_aluno'] = pygame.mixer.Sound('sons/atingealuno.wav')
 
 
 class Letra(pygame.sprite.Sprite):
@@ -59,20 +60,17 @@ class Letra(pygame.sprite.Sprite):
             self.speedy = random.randint(2, 9)
 
 class Aluno(pygame.sprite.Sprite):
-    def __init__(self, img, todos_elementos, todos_avioes, aviao_imagem, hit_aluno, lancamento_aviao):
+    def __init__(self, recursos, grupos):
         # Construtor da classe mãe (Sprite).
         pygame.sprite.Sprite.__init__(self)
-        self.image = img
+        self.image = recursos['jogador_imagem']
         self.rect = self.image.get_rect()
         self.rect.centerx = comprimento/2
         self.rect.bottom = altura - 5
         self.speedx = 0
-        self.todos_elementos = todos_elementos
-        self.todos_avioes = todos_avioes
-        self.aviao_imagem = aviao_imagem
+        self.grupos = grupos
+        self.recursos = recursos
         self.direcao = 1  #Começa olhando pra direita
-        self.hit_aluno = hit_aluno
-        self.lancamento_aviao = lancamento_aviao
     
     def update(self):
         self.rect.x += self.speedx
@@ -85,38 +83,42 @@ class Aluno(pygame.sprite.Sprite):
 
     def lancamento(self):
         if self.direcao == 1:
-            novo_aviao = Aviao(self.aviao_imagem, self.rect.centery,  self.rect.right)
+            novo_aviao = Aviao(self.recursos, self.rect.centery,  self.rect.right)
         else:
-            novo_aviao = Aviao(self.aviao_imagem, self.rect.centery,  (self.rect.left-comprimento_letras))
-        self.todos_elementos.add(novo_aviao) 
-        self.todos_avioes.add(novo_aviao)
-        self.lancamento_aviao.play()
+            novo_aviao = Aviao(self.recursos, self.rect.centery,  (self.rect.left-comprimento_letras))
+        self.grupos['todos_elementos'].add(novo_aviao) 
+        self.grupos['todos_avioes'].add(novo_aviao)
+        self.recursos['lancamento_aviao'].play()
             
 class Aviao(pygame.sprite.Sprite):
-    def __init__(self, img, posicao_y, posicao_x, lancamento):
+    def __init__(self, recursos, posicao_y, posicao_x):
         pygame.sprite.Sprite.__init__(self)
-        self.image = img
+        self.image = recursos['aviao_imagem']
         self.rect = self.image.get_rect()
         self.rect.y = posicao_y
         self.rect.x = posicao_x
-        self.speedy = -5
+        self.speedy = -5  
     
     def update(self):
         self.rect.y += self.speedy
         if self.rect.y < 0:
             self.kill()
 
-
 todos_elementos = pygame.sprite.Group()
 todas_letras = pygame.sprite.Group()
 todos_avioes = pygame.sprite.Group()
 
-jogador = Aluno(jogador_imagem, todos_elementos, todos_avioes, aviao_imagem, hit_aluno, lancamento_aviao)
+grupos = {}
+grupos['todos_elementos'] = todos_elementos
+grupos['todas_letras'] = todas_letras
+grupos['todos_avioes'] = todos_avioes
+
+jogador = Aluno(recursos, grupos)
 todos_elementos.add(jogador)
 
 for a in range(4):
-    letra_I = Letra(letra_I_imagem)
-    letra_D = Letra(letra_D_imagem)
+    letra_I = Letra(recursos['letra_I_imagem'])
+    letra_D = Letra(recursos['letra_D_imagem'])
     todos_elementos.add(letra_I)
     todos_elementos.add(letra_D)
     todas_letras.add(letra_I)
@@ -158,7 +160,7 @@ while game:
     # colisoes = pygame.sprite.spritecollide(jogador, todas_letras, True, True)
     
     colisoes = pygame.sprite.spritecollide(jogador, todas_letras, True)
-    letras = [letra_D_imagem, letra_I_imagem]
+    letras = [recursos['letra_D_imagem'], recursos['letra_I_imagem']]
     for a in colisoes: # As chaves são os elementos do primeiro grupo (meteoros) que colidiram com alguma bala
         # O meteoro e destruido e precisa ser recriado
         random.shuffle(letras)
@@ -168,10 +170,10 @@ while game:
     if len(colisoes)>0:
         #game = False
         print('colidiu')
-        hit_aluno.play()
+        recursos['hit_aluno'].play()
 
     window.fill((0, 0, 0))  # Preenche com a cor preta
-    window.blit(background, (-20, 0))
+    window.blit(recursos['background'], (-20, 0))
 
     todos_elementos.draw(window)
 
