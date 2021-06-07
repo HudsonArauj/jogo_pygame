2b2cd766243a1d8a7d6605d7393cbc716e62d7a1
Hudson Monteiro <hudsonmonteiro2011!gmail.com>
2021-06-06 22:24:03 -0300

    coloquei o jogo e imagens dentro de funções

--- jogo_1.py
+++ jogo_1.py
@@ -21,55 +21,56 @@ altura_aviao = 45
 
 window = pygame.display.set_mode((comprimento, altura))
 pygame.display.set_caption('Diploma Battle')
+def carrega_recurso():
+    recursos = {}
+    recursos['background'] = pygame.image.load('imagens/fachada_insper.jpg').convert()
+    recursos['background'] = pygame.transform.scale(recursos['background'], (comprimento_fundo, altura_fundo))
+    recursos['letra_I_imagem'] = pygame.image.load('imagens/letra_i.png').convert_alpha()
+    recursos['letra_D_imagem'] = pygame.image.load('imagens/letra_d.png').convert_alpha()
+    recursos['letra_I_imagem'] = pygame.transform.scale(recursos['letra_I_imagem'], (comprimento_letras, altura_letras))
+    recursos['letra_D_imagem'] = pygame.transform.scale(recursos['letra_D_imagem'], (comprimento_letras, altura_letras))
+    recursos['aviao_imagem'] = pygame.image.load('imagens/aviao.png').convert_alpha()
+    recursos['aviao_imagem'] = pygame.transform.scale(recursos['aviao_imagem'], (comprimento_aviao, altura_aviao))
+    
+    corridad = []
+    corridae = []
+    professor = []
 
-recursos = {}
-recursos['background'] = pygame.image.load('imagens/fachada_insper.jpg').convert()
-recursos['background'] = pygame.transform.scale(recursos['background'], (comprimento_fundo, altura_fundo))
-recursos['letra_I_imagem'] = pygame.image.load('imagens/letra_i.png').convert_alpha()
-recursos['letra_D_imagem'] = pygame.image.load('imagens/letra_d.png').convert_alpha()
-recursos['letra_I_imagem'] = pygame.transform.scale(recursos['letra_I_imagem'], (comprimento_letras, altura_letras))
-recursos['letra_D_imagem'] = pygame.transform.scale(recursos['letra_D_imagem'], (comprimento_letras, altura_letras))
-recursos['aviao_imagem'] = pygame.image.load('imagens/aviao.png').convert_alpha()
-recursos['aviao_imagem'] = pygame.transform.scale(recursos['aviao_imagem'], (comprimento_aviao, altura_aviao))
+    for i in range(1,11):
+        nome_arquivod = 'imagens/mov{}-d.png'.format(i)
+        nome_arquivoe = 'imagens/mov{}-e.png'.format(i)
+        jogador_imagemd = pygame.image.load(nome_arquivod).convert_alpha()
+        jogador_imagemd = pygame.transform.scale(jogador_imagemd, (comprimento_jogador, altura_jogador))
+        jogador_imageme = pygame.image.load(nome_arquivoe).convert_alpha()
+        jogador_imageme = pygame.transform.scale(jogador_imageme, (comprimento_jogador, altura_jogador))
+        corridad.append(jogador_imagemd)
+        corridae.append(jogador_imageme)
+    recursos['corridad'] = corridad
+    recursos['corridae'] = corridae
 
-corridad = []
-corridae = []
-professor = []
+    i = 0
+    while i < 8:
+        if i <= 3:
+            nome_arquivo2 = 'imagens/prof1.png'
+            professor_imagem = pygame.image.load(nome_arquivo2).convert_alpha()
+            professor_imagem = pygame.transform.scale(professor_imagem, (comprimento_professor, altura_professor))
+            professor.append(professor_imagem)
+        else:
+            nome_arquivo2 = 'imagens/prof2.png'
+            professor_imagem = pygame.image.load(nome_arquivo2).convert_alpha()
+            professor_imagem = pygame.transform.scale(professor_imagem, (comprimento_professor, altura_professor))
+            professor.append(professor_imagem)
+        i += 1
+    recursos['professor_imagem'] = professor
 
-for i in range(1,11):
-    nome_arquivod = 'imagens/mov{}-d.png'.format(i)
-    nome_arquivoe = 'imagens/mov{}-e.png'.format(i)
-    jogador_imagemd = pygame.image.load(nome_arquivod).convert_alpha()
-    jogador_imagemd = pygame.transform.scale(jogador_imagemd, (comprimento_jogador, altura_jogador))
-    jogador_imageme = pygame.image.load(nome_arquivoe).convert_alpha()
-    jogador_imageme = pygame.transform.scale(jogador_imageme, (comprimento_jogador, altura_jogador))
-    corridad.append(jogador_imagemd)
-    corridae.append(jogador_imageme)
-recursos['corridad'] = corridad
-recursos['corridae'] = corridae
+    pygame.mixer.music.load('sons/fundodojogo.mp3')
+    pygame.mixer.music.set_volume(0.2)
+    recursos['hit_professor'] = pygame.mixer.Sound('sons/atingeprof.wav')
+    recursos['lancamento_aviao'] = pygame.mixer.Sound('sons/lançamento.wav')
+    recursos['hit_aluno'] = pygame.mixer.Sound('sons/atingealuno.wav')
 
-i = 0
-while i < 8:
-    if i <= 3:
-        nome_arquivo2 = 'imagens/prof1.png'
-        professor_imagem = pygame.image.load(nome_arquivo2).convert_alpha()
-        professor_imagem = pygame.transform.scale(professor_imagem, (comprimento_professor, altura_professor))
-        professor.append(professor_imagem)
-    else:
-        nome_arquivo2 = 'imagens/prof2.png'
-        professor_imagem = pygame.image.load(nome_arquivo2).convert_alpha()
-        professor_imagem = pygame.transform.scale(professor_imagem, (comprimento_professor, altura_professor))
-        professor.append(professor_imagem)
-    i += 1
-recursos['professor_imagem'] = professor
-
-pygame.mixer.music.load('sons/fundodojogo.mp3')
-pygame.mixer.music.set_volume(0.2)
-recursos['hit_professor'] = pygame.mixer.Sound('sons/atingeprof.wav')
-recursos['lancamento_aviao'] = pygame.mixer.Sound('sons/lançamento.wav')
-recursos['hit_aluno'] = pygame.mixer.Sound('sons/atingealuno.wav')
-
-recursos["fonte_placar"] = pygame.font.Font('fonte/PressStart2P.ttf', 18)
+    recursos["fonte_placar"] = pygame.font.Font('fonte/PressStart2P.ttf', 18)
+    return recursos
 
 class Letra(pygame.sprite.Sprite):
     def __init__(self, img):
@@ -173,116 +174,118 @@ class Professor(pygame.sprite.Sprite):
     def update(self):
         self.pos = (self.pos+1)%len(self.recursos['professor_imagem']) #faz as imagens formarem um loop
         self.image = self.recursos['professor_imagem'][self.pos]
+def tela_jogo(window):
+    clock = pygame.time.Clock() #Ajustando a velocidade
+    recursos = carrega_recurso()
+    todos_elementos = pygame.sprite.Group()
+    todas_letras = pygame.sprite.Group()
+    todos_avioes = pygame.sprite.Group()
 
-todos_elementos = pygame.sprite.Group()
-todas_letras = pygame.sprite.Group()
-todos_avioes = pygame.sprite.Group()
+    grupos = {}
+    grupos['todos_elementos'] = todos_elementos
+    grupos['todas_letras'] = todas_letras
+    grupos['todos_avioes'] = todos_avioes
 
-grupos = {}
-grupos['todos_elementos'] = todos_elementos
-grupos['todas_letras'] = todas_letras
-grupos['todos_avioes'] = todos_avioes
+    chefe = Professor(recursos, grupos)
+    todos_elementos.add(chefe)
+    jogador = Aluno(recursos, grupos)
+    todos_elementos.add(jogador)
 
-chefe = Professor(recursos, grupos)
-todos_elementos.add(chefe)
-jogador = Aluno(recursos, grupos)
-todos_elementos.add(jogador)
+    for a in range(3):
+        letra_I = Letra(recursos['letra_I_imagem'])
+        letra_D = Letra(recursos['letra_D_imagem'])
+        todos_elementos.add(letra_I)
+        todos_elementos.add(letra_D)
+        todas_letras.add(letra_I)
+        todas_letras.add(letra_D)
 
-for a in range(3):
-    letra_I = Letra(recursos['letra_I_imagem'])
-    letra_D = Letra(recursos['letra_D_imagem'])
-    todos_elementos.add(letra_I)
-    todos_elementos.add(letra_D)
-    todas_letras.add(letra_I)
-    todas_letras.add(letra_D)
+    pygame.mixer.music.play(loops=-1)
 
-pygame.mixer.music.play(loops=-1)
+    ACABOU = 0
+    JOGANDO = 1
+    COLIDINDO = 2
+    estado = JOGANDO
 
-ACABOU = 0
-JOGANDO = 1
-COLIDINDO = 2
-estado = JOGANDO
+    tecla_apertada = {}
+    placar = 0
+    vidas = 5
 
-tecla_apertada = {}
-placar = 0
-vidas = 5
+    FPS = 30
+    v = 8
 
-clock = pygame.time.Clock() #Ajustando a velocidade
-FPS = 30
-v = 8
+    #Loop principal
+    while estado != ACABOU:
+        clock.tick(FPS)
 
-#Loop principal
-while estado != ACABOU:
-    clock.tick(FPS)
-
-    for event in pygame.event.get():
-        
-        if event.type == pygame.QUIT:
-            estado = ACABOU
-        if estado == JOGANDO:
-            if event.type == pygame.KEYDOWN:
-                tecla_apertada[event.key] = True
-                if event.key == pygame.K_LEFT:
-                    jogador.speedx -= v
-                    jogador.direcao = 0
-                if event.key == pygame.K_RIGHT:
-                    jogador.speedx += v
-                    jogador.direcao = 1
-                if event.key == pygame.K_SPACE:
-                    jogador.lancamento()
-        if event.type == pygame.KEYUP:
-            if event.key in tecla_apertada and tecla_apertada[event.key]:
-                if event.key == pygame.K_LEFT:
-                    jogador.speedx += v
-                if event.key == pygame.K_RIGHT:
-                    jogador.speedx -= v
-
-    todos_elementos.update()
-    #verifica se tem colisões com as letras
-
-    if estado == JOGANDO:
-        colisao_avi = pygame.sprite.spritecollide(chefe, todos_avioes, True, pygame.sprite.collide_mask)
-        if len(colisao_avi)>0:
-            recursos['hit_professor'].play()
-            placar += 10
+        for event in pygame.event.get():
             
-        colisoes = pygame.sprite.spritecollide(jogador, todas_letras, True, pygame.sprite.collide_mask)
-        letras = [recursos['letra_D_imagem'], recursos['letra_I_imagem']]
-        for a in colisoes: # As chaves são os elementos do primeiro grupo (meteoros) que colidiram com alguma bala
-            # O meteoro e destruido e precisa ser recriado
-            random.shuffle(letras)
-            imagem = Letra(letras[0])
-            todos_elementos.add(imagem)
-            todas_letras.add(imagem)
+            if event.type == pygame.QUIT:
+                estado = ACABOU
+            if estado == JOGANDO:
+                if event.type == pygame.KEYDOWN:
+                    tecla_apertada[event.key] = True
+                    if event.key == pygame.K_LEFT:
+                        jogador.speedx -= v
+                        jogador.direcao = 0
+                    if event.key == pygame.K_RIGHT:
+                        jogador.speedx += v
+                        jogador.direcao = 1
+                    if event.key == pygame.K_SPACE:
+                        jogador.lancamento()
+            if event.type == pygame.KEYUP:
+                if event.key in tecla_apertada and tecla_apertada[event.key]:
+                    if event.key == pygame.K_LEFT:
+                        jogador.speedx += v
+                    if event.key == pygame.K_RIGHT:
+                        jogador.speedx -= v
 
-        if len(colisoes)>0:
-            recursos['hit_aluno'].play()
-            jogador.kill()
-            vidas -= 1
-            estado = COLIDINDO
-            tecla_apertada = {}
-    elif estado == COLIDINDO:
-        if vidas == 0:
-            estado = ACABOU
-        else:
-            estado = JOGANDO
-            jogador = Aluno(recursos, grupos)
-            todos_elementos.add(jogador)
+        todos_elementos.update()
+        #verifica se tem colisões com as letras
+
+        if estado == JOGANDO:
+            colisao_avi = pygame.sprite.spritecollide(chefe, todos_avioes, True, pygame.sprite.collide_mask)
+            if len(colisao_avi)>0:
+                recursos['hit_professor'].play()
+                placar += 10
+                
+            colisoes = pygame.sprite.spritecollide(jogador, todas_letras, True, pygame.sprite.collide_mask)
+            letras = [recursos['letra_D_imagem'], recursos['letra_I_imagem']]
+            for a in colisoes: # As chaves são os elementos do primeiro grupo (meteoros) que colidiram com alguma bala
+                # O meteoro e destruido e precisa ser recriado
+                random.shuffle(letras)
+                imagem = Letra(letras[0])
+                todos_elementos.add(imagem)
+                todas_letras.add(imagem)
+
+            if len(colisoes)>0:
+                recursos['hit_aluno'].play()
+                jogador.kill()
+                vidas -= 1
+                estado = COLIDINDO
+                tecla_apertada = {}
+        elif estado == COLIDINDO:
+            if vidas == 0:
+                estado = ACABOU
+            else:
+                estado = JOGANDO
+                jogador = Aluno(recursos, grupos)
+                todos_elementos.add(jogador)
     
-    window.fill((0, 0, 0))  # Preenche com a cor preta
-    window.blit(recursos['background'], (-20, 0))
-    todos_elementos.draw(window)
+        window.fill((0, 0, 0))  # Preenche com a cor preta
+        window.blit(recursos['background'], (-20, 0))
+        todos_elementos.draw(window)
 
-    texto_superficie = recursos['fonte_placar'].render("{:05d}".format(placar), True, (255, 255, 0))
-    text_rect = texto_superficie.get_rect()
-    text_rect.midtop = (50,  10)
-    window.blit(texto_superficie, text_rect)
+        texto_superficie = recursos['fonte_placar'].render("{:05d}".format(placar), True, (255, 255, 0))
+        text_rect = texto_superficie.get_rect()
+        text_rect.midtop = (50,  10)
+        window.blit(texto_superficie, text_rect)
 
-    texto_superficie = recursos['fonte_placar'].render(chr(9829) * vidas, True, (255, 0, 0))
-    text_rect = texto_superficie.get_rect()
-    text_rect.bottomleft = (10, altura - 10)
-    window.blit(texto_superficie, text_rect)
-    #Atualiza jogo
-    pygame.display.update()  
+        texto_superficie = recursos['fonte_placar'].render(chr(9829) * vidas, True, (255, 0, 0))
+        text_rect = texto_superficie.get_rect()
+        text_rect.bottomleft = (10, altura - 10)
+        window.blit(texto_superficie, text_rect)
+        #Atualiza jogo
+        pygame.display.update()  
+tela_jogo(window)
 
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
